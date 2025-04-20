6c6e1281831b4acde2126e957cd34b13c5fdc8be
Yared Mekonnen <yaredmekonnen999@gmail.com>
2025-04-19 21:56:45 -0500

    matching parenthesis using a linkedliststack

---
+++ Stack/MatchParenthesis.java
@@ -0,0 +1,21 @@
+package Stack;
+
+public class MatchParenthesis {
+  private static boolean parenthesisMatches(String expression) {
+
+    LinkedListStack<Character> buffer = new LinkedListStack<>();
+    final String opening = "({[";
+    final String closing = ")}]";
+    for (char c : expression.toCharArray()) {
+      if (opening.indexOf(c) != -1) {
+        buffer.push(c);
+      } else if (closing.indexOf(c) != -1) {
+        if (buffer.isEmpty())
+          return false;
+        if (closing.indexOf(c) != opening.indexOf(buffer.pop()))
+          return false;
+      }
+    }
+    return buffer.isEmpty();
+  }
+}
