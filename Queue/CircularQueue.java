8fc2a9207094b6a62f99cddfb7292b960c9e9d9f
Yared Mekonnen <yaredmekonnen999@gmail.com>
2025-04-22 23:59:08 -0500

    fixed the rotate method in the queue interface but still needs another fix on implementing a remove first method in the circular linked list

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -1,37 +1,5 @@
 package Queue;
 
-import CircularlyLinkedList.CircularlyLinkedList;
-
-public class CircularQueue<E> implements Queue<E> {
-
-  private CircularlyLinkedList<E> Cqueue;
-
-  public CircularQueue() {
-    Cqueue = new CircularlyLinkedList<E>();
-  }
-
-  public void enqueue(E e) {
-    Cqueue.addLast(e);
-  }
-
-  public E dequeue() {
-    return Cqueue.getFirst();
-  }
-
-  public E first() {
-    return Cqueue.getFirst();
-  }
-
-  public int size() {
-    return Cqueue.getSize();
-  }
-
-  public boolean isEmpty() {
-    return Cqueue.isEmpty();
-  }
-
-  public void rotate() {
-    Cqueue.rotate();
-  }
-
+public interface CircularQueue<E> extends Queue<E> {
+  void rotate();
 }
---
+++ Queue/LinkedCircularQueue.java
@@ -0,0 +1,37 @@
+package Queue;
+
+import CircularlyLinkedList.CircularlyLinkedList;
+
+public class LinkedCircularQueue<E> implements CircularQueue<E> {
+  private CircularlyLinkedList<E> circularlinkedlist;
+
+  public LinkedCircularQueue() {
+    circularlinkedlist = new CircularlyLinkedList<E>();
+  }
+
+  public void enqueue(E e) {
+    circularlinkedlist.addLast(e);
+  }
+
+  public E dequeue() {
+    return circularlinkedlist.getFirst();
+  }
+
+  public E first() {
+    return circularlinkedlist.getFirst();
+  }
+
+  public int size() {
+    return circularlinkedlist.getSize();
+
+  }
+
+  public boolean isEmpty() {
+    return circularlinkedlist.isEmpty();
+  }
+
+  public void rotate() {
+    circularlinkedlist.rotate();
+  }
+
+}
