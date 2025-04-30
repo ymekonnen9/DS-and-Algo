a7036e0cead4a02744321673ed7c8af9d6221bcd
Yared Mekonnen <yaredmekonnen999@gmail.com>
2025-04-29 22:29:04 -0500

    Abstract Tree implementation

---
+++ Trees/AbstractTree.java
@@ -0,0 +1,27 @@
+package Trees;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+  public boolean isRoot(Position<E> p) throws IllegalArgumentException {
+    return p == root();
+  };
+
+  public boolean isInternal(Position<E> p) throws IllegalArgumentException {
+    return numChildren(p) == 0;
+  };
+
+  public boolean isExternal(Position<E> p) throws IllegalArgumentException {
+    return numChildren(p) > 0;
+  };
+
+  public boolean isEmpty() {
+    return size() == 0;
+  };
+
+  public int depth(Position<E> p) {
+    if (isRoot(p)) {
+      return 0;
+    } else {
+      return 1 + depth(parent(p));
+    }
+  }
+}
